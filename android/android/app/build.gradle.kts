5bdabe2e2a26ff1ee137dea73e3a929c36b0c6ee
Iarrhi0 <Iarrhi0@users.noreply.github.com>
2026-09-04 11:33:00 -0700

    Anime Imposteur V6.2 multiplayer AI PWA Android

--- android/android/app/build.gradle.kts
+++ android/android/app/build.gradle.kts
@@ -1,4 +1,4 @@
-﻿plugins {
+plugins {
     id("com.android.application")
 }
 android {
@@ -10,7 +10,7 @@ android {
         targetSdk = 35
         versionCode = 6
         versionName = "6.0"
-        buildConfigField("String","GAME_URL","\"https://Iarrhi0.github.io/anime-imposteur-multijoueur-20260904-100307/\"")
+        buildConfigField("String","GAME_URL","\"__GAME_URL__\"")
     }
     buildFeatures { buildConfig = true }
     compileOptions {
@@ -22,5 +22,3 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.activity:activity:1.10.0")
 }
-
-
---
+++ android/android/app/src/main/java/com/animeimposteur/app/MainActivity.java
@@ -0,0 +1,55 @@
+package com.animeimposteur.app;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+        webView.setBackgroundColor(Color.rgb(7,17,31));
+
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setMediaPlaybackRequiresUserGesture(false);
+
+        CookieManager.getInstance().setAcceptCookie(true);
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView,true);
+
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest req) {
+                return false;
+            }
+        });
+
+        getOnBackPressedDispatcher().addCallback(this,new OnBackPressedCallback(true) {
+            @Override public void handleOnBackPressed() {
+                if(webView.canGoBack()) webView.goBack(); else finish();
+            }
+        });
+
+        if(savedInstanceState==null) webView.loadUrl(BuildConfig.GAME_URL);
+        else webView.restoreState(savedInstanceState);
+    }
+
+    @Override protected void onSaveInstanceState(Bundle out) {
+        webView.saveState(out);
+        super.onSaveInstanceState(out);
+    }
+}
